d75c9d77ce67107ed354372ed8e5b06e5105d10d
uint256_t <konndennsa@gmail.com>
2017-03-11 14:47:47 +0900

    add class 'Tourmaline'

--- src/tourmaline.cpp
+++ src/tourmaline.cpp
@@ -1,3 +1,24 @@
 #include "tourmaline.hpp"
 
+Tourmaline::Tourmaline() {
+}
 
+
+int Tourmaline::run(int _argc, char *_argv[]) {
+  argc = _argc; argv = _argv;
+  
+  if(argc < 2) {
+    show_usage();
+    return 0;
+  }
+
+  return run_from_file(argv[1]);
+}
+
+int Tourmaline::run_from_file(std::string file_name) {
+  return 0;
+}
+
+void Tourmaline::show_usage() {
+  puts("./tourmaline <file>");
+}
--- src/tourmaline.hpp
+++ src/tourmaline.hpp
@@ -2,4 +2,16 @@
 
 #include "common.hpp"
 
+class Tourmaline {
+  private:
+    int argc;
+    char **argv;
 
+  public:
+    Tourmaline();
+
+    int run(int, char **);
+    void show_usage();
+
+    int run_from_file(std::string);
+};
