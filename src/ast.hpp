7ef21cc15ad8182513db22facfe472e3e9637a6e
uint256_t <konndennsa@gmail.com>
2017-03-14 21:18:19 +0900

    create accessor of AST member variables

--- src/ast.hpp
+++ src/ast.hpp
@@ -31,58 +31,74 @@ typedef std::vector<func_arg_t *> func_args_t;
 
 class FuncDefAST : public AST {
   private:
-  public:
     std::string name;
     func_args_t args;
     AST_vec body;
+  public:
     FuncDefAST(std::string name, func_args_t args, AST_vec body);
     virtual int get_kind() const { return AST_FUNC_DEF; };
     virtual void show();
+
+    std::string get_name() const { return name; };
+    func_args_t get_args() const { return args; };
+    AST_vec     get_body() const { return body; };
 };
 
 class FuncCallAST : public AST {
   private:
-  public:
     AST *callee;
     AST_vec args;
+  public:
     FuncCallAST(AST *callee, AST_vec args);
     virtual int get_kind() const { return AST_FUNC_CALL; };
     virtual void show();
+    AST    *get_callee() const { return callee; };
+    AST_vec get_args()   const { return args; };
 };
 
 class BinaryOpAST : public AST {
   private:
-  public:
     AST *lhs, *rhs;
     std::string op;
+  public:
     BinaryOpAST(std::string op, AST *lhs, AST *rhs);
     virtual int get_kind() const { return AST_BINARY_OP; };
     virtual void show();
+
+    AST         *get_lhs() const { return lhs; };
+    AST         *get_rhs() const { return rhs; };
+    std::string  get_op()  const { return op; };
 };
 
 class VariableAST : public AST {
   private:
-  public:
     std::string name;
+  public:
     VariableAST(std::string);
     virtual int get_kind() const { return AST_VARIABLE; };
     virtual void show();
+
+    std::string get_name() const { return name; };
 };
 
 class INumberAST : public AST {
   private:
-  public:
     int number;
+  public:
     INumberAST(int);
     virtual int get_kind() const { return AST_INUMBER; };
     virtual void show();
+
+    int get_number() const { return number; };
 };
 
 class StringAST : public AST {
   private:
-  public:
     std::string str;
+  public:
     StringAST(std::string);
     virtual int get_kind() const { return AST_STRING; };
     virtual void show();
+
+    std::string get_str() const { return str; };
 };
